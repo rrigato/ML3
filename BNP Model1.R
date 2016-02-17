e601744080d723eb1129b233e10bfcf9e88d6815
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-02-16 19:11:35 -0800

    Fixed log_loss function to work with binary outcome variables

--- BNP Model1.R
+++ BNP Model1.R
@@ -6,27 +6,39 @@
 #
 #######################################################
 
+#rename function
+library(plyr)
+
+
+
 #import datasets
 train <- read.csv('C:\\Users\\Randy\\Downloads\\Kaggle BNP\\train.csv')
 test <- read.csv('C:\\Users\\Randy\\Downloads\\Kaggle BNP\\test.csv')
 
 
-bothFrames = split(train)
-str(bothFrames)
+#sets the working directory
+setwd('C:/Users/Randy/Downloads/Kaggle BNP/ML3')
 
+#Includes functions in the BNP Function script
+source("BNP functions.R")
+
+
+#calls the split function to divide the train dataset
+bothFrames = split(train)
 train2 = bothFrames[[1]]
 test2 = bothFrames[[2]]
 
 
 
+outputFrame = data.frame(matrix(nrow= nrow(test2), ncol=2))
+outputFrame = rename(outputFrame, c("X1" = "ID", "X2" = "PredictedProb"))
+
+outputFrame = cbind(outputFrame, test2$target)
+outputFrame[,1] = test2[,1]
+outputFrame[,2] = .5
 
 
-
-
-
-
-
-
+log_loss(outputFrame)
 
 
 
@@ -53,32 +65,27 @@ test2 = bothFrames[[2]]
 
 ###############################################################
 #
-#The log_loss function takes two arguements, a data.frame and
-#a vector of length 1 num_predict
-#The data.frame has ncol = num_predict +1  with column 1 = id, column 2 = predict_0, 
-#column 3 = predict_1 ... etc
+#The log_loss function takes one arguements, a data_frame which has the 
+#id, predictions, and actual
 #
-#It will multiply the log of the predicted probability times 1 if the observation
-#turned out to be that category, 0 otherwise
-#It sums all of those up and divides by -N, where N is the number of 
-#observations in data_frame
+#
+#only works for binary outcome. For multicategory logloss, refer to Kaggle Telstra script
 #
 #################################################################
-log_loss <- function(data_frame, num_predict)
+log_loss <- function(data_frame)
 {
 	total = 0
 	for( i in 1:nrow(data_frame))
 	{
 		
-		for(j in 1: num_predict)
-		{
+
 			y=0
 			#gets the id from the ith row of the data_frame
 			#if the actual fault_severity == j-1 then that is when y is 1
 			#This is the classification of the point
 			#The [1] just takes the first in case their are duplicate observations
 			#of the id
-			if (test2[which(test2$id==data_frame$id[i])[1],3] == (j-1))
+			if (data_frame[i,3] == 1)
 			{
 				y=1;
 				
@@ -87,10 +94,10 @@ log_loss <- function(data_frame, num_predict)
 			#total is equal to total plus y times
 			# the log of the ith row and the j+1 column
 			#it is j+1 because predict_0 is in column 2
-			total = total + y*log( max( min( data_frame[i,(j+1)], 1-10^(-15) ),  10^(-15) ) )
+			total = total + y*log( max( min( data_frame[i,2], 1-10^(-15) ),  10^(-15) ) ) +
+				(1-y)*log( max( min((1- data_frame[i,2]), 1-10^(-15) ),  10^(-15) ) )
 			
 
-		}
 		
 	}
 	print("Your logloss score is:")
--- BNP functions.R
+++ BNP functions.R
@@ -49,6 +49,53 @@ split <- function(train)
 
 
 
+###############################################################
+#
+#The log_loss function takes one arguements, a data_frame which has the 
+#id, predictions, and actual
+#
+#
+#only works for binary outcome. For multicategory logloss, refer to Kaggle Telstra script
+#
+#################################################################
+log_loss <- function(data_frame)
+{
+	total = 0
+	for( i in 1:nrow(data_frame))
+	{
+		
+
+			y=0
+			#gets the id from the ith row of the data_frame
+			#if the actual fault_severity == j-1 then that is when y is 1
+			#This is the classification of the point
+			#The [1] just takes the first in case their are duplicate observations
+			#of the id
+			if (data_frame[i,3] == 1)
+			{
+				y=1;
+				
+			}
+
+			#total is equal to total plus y times
+			# the log of the ith row and the j+1 column
+			#it is j+1 because predict_0 is in column 2
+			total = total + y*log( max( min( data_frame[i,2], 1-10^(-15) ),  10^(-15) ) ) +
+				(1-y)*log( max( min((1- data_frame[i,2]), 1-10^(-15) ),  10^(-15) ) )
+			
+
+		
+	}
+	print("Your logloss score is:")
+	print(-total/nrow(data_frame))
+
+
+}
+
+
+
+
+
 
 
 
