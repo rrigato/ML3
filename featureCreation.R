4547115291f8a3d9ed06733856251def37043437
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-03-04 16:36:05 -0800

    Added some new features based on the graphs

--- featureCreation.R
+++ featureCreation.R
@@ -14,7 +14,7 @@ train <- read.csv('C:\\Users\\Randy\\Downloads\\Kaggle BNP\\train.csv')
 test <- read.csv('C:\\Users\\Randy\\Downloads\\Kaggle BNP\\test.csv')
 
 #plot the variables
-for( i in 3:134)
+for( i in 15:134)
 {
 	if(i %%6 == 3)
 	{
@@ -33,5 +33,101 @@ test[,133:150] = 0
 #number of NAs in each row is a variable
 train[1:nrow(train),134] = rowSums(is.na(train))
 test[1:nrow(test),134] = rowSums(is.na(test))
+
+#interaction between v5 and v4
+train[,135] = train[,6] * train[,7]
+test[,135] = test[,5] * test[,6]
+
+
+#1 v5*v4 <=20
+train[which(train[,135] <= 20),136] = 1
+test[which(test[,135] <= 20),136]  = 1
+
+
+
+
+#1 v5*v4 >20 <= 40
+train[which(train[,135] > 20 & train[,135] <= 40 ), 137] = 1
+test[which(test[,135]   > 20  & test[,135] <= 40),  137] = 1
+
+
+
+#1 v5*v4 >40 <= 60
+train[which(train[,135] > 40 & train[,135] <= 60 ), 138] = 1
+test[which(test[,135]   > 40  & test[,135] <= 60),  138] = 1
+
+
+
+#1 v5*v4 >40 <= 60
+train[which(train[,135] > 60  ), 139] = 1
+test[which(test[,135]   > 60  ),  139] = 1
+
+
+
+#interaction between v11 and v12
+train[,140] = train[,13]*train[,14]
+test[,140] = test[,12]*test[,13]
+
+
+#v11*v12 <=100
+train[which(train[,140] <= 100  ),141] = 1
+test[which(test[,140]   <= 100 ),  141] = 1
+
+#v11*v12 > 100
+train[which(train[,140] > 100  ), 142] = 1
+test[which(test[,140]   > 100 ),  142] = 1
+
+
+
+#interaction between v15 and v16
+train[,143] = train[,17]*train[,18]
+test[,143] = test[,16]*test[,17]
+
+
+
+#v15*v16 <=7
+train[which(train[,143] <= 7  ), 144] = 1
+test[which(test[,143]   <= 7 ),  144] = 1
+
+
+#v15*v16 >7 <=12
+train[which(train[,143] > 7 & train[,143] <= 12 ), 145] = 1
+test[which(test[,143]   > 7  & test[,143] <= 12),  145] = 1
+
+
+#v15*v16 <=7
+train[which(train[,143] > 12  ), 146] = 1
+test[which(test[,143]   > 12 ),  146] = 1
+
+
+#v27*v28
+train[,147] = train[,29]*train[,30]
+test[,147] = test[,28]*test[,29]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
